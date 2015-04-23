8c2eb849c4fb6fd5ed69cd40507f2d54056c3ec1
GrSpirit <gr.spirit@gmail.com>
2015-04-23 18:08:58 +0300

    Added wagons

--- app/controllers/wagons_controller.rb
+++ app/controllers/wagons_controller.rb
@@ -1,11 +1,25 @@
 class WagonsController < ApplicationController
+	def index
+		@wagons = Wagon.all
+	end
+
+	def show
+		@wagon = Wagon.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
-		@wagon = Wagon.new(params[:wagon])
+		@wagon = Wagon.new(wagon_params)
 
 		@wagon.save
 		redirect_to @wagon
 	end
+
+private
+	def wagon_params
+		params.require(:wagon).permit(:label, :description, :created_at, :repared_at)
+	end
+
 end
