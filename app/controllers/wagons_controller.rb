460fcd08e5fcfe468517519985f14ad91bee37f6
GrSpirit <gr.spirit@gmail.com>
2015-04-24 09:25:41 +0300

    Added model Comment

--- app/controllers/wagons_controller.rb
+++ app/controllers/wagons_controller.rb
@@ -1,43 +1,50 @@
 class WagonsController < ApplicationController
-	def index
-		@wagons = Wagon.all
-	end
+  def index
+    @wagons = Wagon.all
+  end
 
-	def show
-		@wagon = Wagon.find(params[:id])
-	end
+  def show
+    @wagon = Wagon.find(params[:id])
+  end
 
-	def new
-		@wagon = Wagon.new
-	end
+  def new
+    @wagon = Wagon.new
+  end
 
-	def edit
-		@wagon = Wagon.find(params[:id])
-	end
+  def edit
+    @wagon = Wagon.find(params[:id])
+  end
 
-	def create
-		@wagon = Wagon.new(wagon_params)
+  def create
+    @wagon = Wagon.new(wagon_params)
 
-		if @wagon.save
-			redirect_to @wagon
-		else
-			render 'new'
-		end
-	end
+    if @wagon.save
+      redirect_to @wagon
+    else
+      render 'new'
+    end
+  end
 
-	def update
-		@wagon = Wagon.find(params[:id])
+  def update
+    @wagon = Wagon.find(params[:id])
 
-		if @wagon.update(wagon_params)
-			redirect_to @wagon
-		else
-			render 'edit'
-		end
-	end
+    if @wagon.update(wagon_params)
+      redirect_to @wagon
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @wagon = Wagon.find(params[:id])
+    @wagon.destroy
+    
+    redirect_to wagons_path
+  end
 
 private
-	def wagon_params
-		params.require(:wagon).permit(:label, :description, :created_at, :repared_at)
-	end
+  def wagon_params
+    params.require(:wagon).permit(:label, :description, :created_at, :repared_at)
+  end
 
 end
