767f441b57bb9b34d980564ea7c04b775648cf98
Vitaliy Shcherbakov <gr.spirit@gmail.com>
2015-04-28 22:31:02 +0300

    DetailModel moved to AJAX

--- app/controllers/detail_models_controller.rb
+++ app/controllers/detail_models_controller.rb
@@ -4,19 +4,38 @@ class DetailModelsController < ApplicationController
   end
   
   def edit
-    @detail_type = @detail_model.detail_type
+    respond_to do |format|
+      format.html
+      format.js { render partial: 'list_items/edit', object: @detail_model, as: 'item' }
+      format.json { render json: @detail_model }
+    end
   end
 
   def create
     @detail_type = DetailType.find(params[:detail_type_id])
-    @detail_type.detail_models.create(detail_model_params)
+    @detail_model = @detail_type.detail_models.create(detail_model_params)
 
-    redirect_to @detail_type
+    if @detail_model.valid?
+      respond_to do |format|
+        format.html { redirect_to @detail_type }
+        format.js { render partial: 'list_items/create', object: @detail_model, as: 'item' }
+        format.json { render json: @detail_model, status: :created, location: @detail_model }
+      end
+    else
+      respond_to do |format|
+        format.html { render @detail_type }
+        format.json { render json: @detail_model.errors, status: :unprocessable_entity }
+        format.js { render partial: 'list_items/new', object: @detail_model, as: 'item' }
+      end
+    end
   end
 
   def update
     if @detail_model.update(detail_model_params)
-      redirect_to @detail_model.detail_type
+      respond_to do |format|
+        format.html { redirect_to @detail_model.detail_class }
+        format.js { render partial: 'list_items/update', object: @detail_model, as: 'item' }
+      end
     else
       render :edit
     end
@@ -27,7 +46,7 @@ class DetailModelsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to @detail_model.detail_type, notice: "Detail type '#{@detail_model.name}' was successfully destroyed." }
       format.json { head :no_content }
-      format.js { render :detail_model }
+      format.js { render partial: 'list_items/destroy', object: @detail_model, as: 'item' }
     end
   end
 
